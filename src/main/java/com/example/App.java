ce5823a99135766963ab8c44244036d4cc9d5a66
partha2212 <riskstrikeback@gmail.com>
2025-06-09 01:58:42 +0530

    first commit:myApp

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,17 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    int a,b,res;
+    a=10;
+    b=20;
+    res= a+b;
+        System.out.println("sum is"+res);
+    }
+}
